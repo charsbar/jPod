a37cfc499052fb6d7b4d94f35d1675b9ebeb3b21
Kenichi Ishigaki <ishigaki@cpan.org>
2010-04-17 14:05:19 +0900

    initial import

---
+++ lib/jPod.pm
@@ -0,0 +1,34 @@
+package jPod;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.02';
+
+1;
+
+__END__
+
+=head1 NAME
+
+jPod - Japanese pod management toolset
+
+=head1 DESCRIPTION
+
+This was originally a tool I used while translating L<Moose::Manual>
+for the sake of the Japan Perl Association in 2009.
+
+See L<http://perldoc.tcool.org/> for web interface.
+
+=head1 AUTHOR
+
+Kenichi Ishigaki, E<lt>ishigaki@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009-2010 by Kenichi Ishigaki.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
